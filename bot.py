ac8435d1ea810831186ff3114190d2cc5b7ca3fd
Shade-MC <554scout@gmail.com>
2020-12-14 17:55:28 -0800

    added bullying

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # bot.py
 import os
 
+import time
+import random
 import discord
 from discord import VoiceState
 from discord.ext.commands import Bot
@@ -13,6 +15,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = Bot(command_prefix='!')
 
+insults = []
+with open('insults.txt') as file:
+    for line in file:
+        insults.append(line)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -21,6 +28,7 @@ async def on_ready():
             if channel.user_limit == 1:
                 lonely[guild] = channel
                 print(f'{channel.name} is desinated as lonely')
+
 
 @client.event
 async def on_voice_state_update(member,before,after):
@@ -38,7 +46,6 @@ async def on_voice_state_update(member,before,after):
             elif len(lonely[after.channel.guild].members) == 1:
                 await lonely[after.channel.guild].members[0].move_to(after.channel)
                 print(f'{member.nick} sent to a friend')
-
     #on user change channel
     elif before.channel is not after.channel:
         if len(after.channel.members) == 1:
@@ -47,5 +54,23 @@ async def on_voice_state_update(member,before,after):
         elif len(before.channel.members) == 1:
             await before.channel.members[0].move_to(lonely[before.channel.guild])
             print(f'{member.nick} sent to the lonely corner')
+
+
+@client.command()
+async def bully(ctx):
+    if random.choice(range(0,100)) == 100:
+        if len(ctx.message.mentions) == 0:
+            await ctx.send("{} {}".format(ctx.author.mention, " is the coolest"))
+        else:
+            await ctx.send("{} {}".format(ctx.message.mentions[0].mention, " is the coolest"))
+    else:
+        if len(ctx.message.mentions) == 0:
+            await ctx.send("{} {}".format(ctx.author.mention, random.choice(insults)))
+        else:
+            await ctx.send("{} {}".format(ctx.message.mentions[0].mention, random.choice(insults)))
+
+@client.command()
+async def gay(ctx):
+    await ctx.send(file=discord.File('youGay.png'))
 
 client.run(TOKEN)
